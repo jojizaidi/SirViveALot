1cc17043c739a2bf9a4fd94acee7fb32c2a133d4
Bilalmirza <Bilalmirza@Bilals-MacBook-Pro.local>
2015-04-30 11:03:47 +0500

    dpad controls

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -53,7 +53,26 @@ bool HelloWorld::init()
     
     this->scheduleUpdate();
     
+    auto s = Director::getInstance()->getWinSize();
     
+    Camera* hudCamera = Camera::createOrthographic(s.width, s.height, 1, 1000);
+    this->addChild(hudCamera);
+    hudCamera->setCameraFlag(CameraFlag::USER2);
+
+    hudLayer = Layer::create();
+    this->addChild(hudLayer);
+    hudLayer->setCameraMask((unsigned int)CameraFlag::USER1);
+    bgJoystick = Sprite::create("dpadbase.png");
+    hudLayer->addChild(bgJoystick,5);
+    bgJoystick->setPosition(50,50);
+    
+        
+    bgJoystick->setScale(0.4);
+    bgJoystick->setPosition(100,100);
+    joystick = Sprite::create("dpadmid.png");
+    hudLayer->addChild(joystick,6);
+    joystick->setScale(0.3);
+
     
     Sprite *test = Sprite::create("HelloWorld.png");
     this->addChild(test);
@@ -67,24 +86,70 @@ void HelloWorld::InitIsland()
 {
     theIsland = Sprite3D::create("mountain1.c3b");
     theIsland->setTexture("mountainsUV.png");
-    theIsland->setPosition(Vec2(winSize.width/2 , winSize.height/2));
+    theIsland->setPosition3D((Vec3(winSize.width/2 , winSize.height/2, - 100)));
     theIsland->setCameraMask((unsigned int)CameraFlag::USER1);
     this->addChild(theIsland);
     theIsland->setScale(5);
+    
 }
+void HelloWorld::onTouchesBegan(const std::vector<Touch*>& touches, Event* event)
+{
+    
+       
+    Point location = touches[0]->getLocation();
+    
+    touchLocation = location;
+    allowMovement = true;
+    
+    if(touchLocation.x < winSize.width/2){
+        joystick->setPosition(touchLocation);
+        bgJoystick->setPosition(touchLocation.x+ 0.01,touchLocation.y + 0.01);
+        
+        joystick->setVisible(true);
+        bgJoystick->setVisible(true);
+        
+        
+        playerRotation =calcAngle(Player::getInstance()->getPosition(), touchLocation) - 90;
+    }
+}
+void HelloWorld::onTouchesMoved(const std::vector<Touch*>& touches, Event* event)
+{
+    
+    Point location = touches[0]->getLocation();
+    touchLocation = location;
+    joystick->setPosition(location);
+    
+    if(joystick->getPosition().distance(bgJoystick->getPosition()) > 30)
+    {
+        float angle = calcAngle(joystick->getPosition(), bgJoystick->getPosition());
+        joystick->setPosition(Point(bgJoystick->getPosition().x  + 30*sin(CC_DEGREES_TO_RADIANS(angle- 90)), bgJoystick->getPosition().y + 30 *cos(CC_DEGREES_TO_RADIANS(angle - 90))));
+    }
+    
+    touchLocation.x = touchLocation.x + Player::getInstance()->getPosition().x - winSize.width/2;
+    touchLocation.y = touchLocation.y + Player::getInstance()->getPosition().y - winSize.height/2;
+    
+    
+    playerRotation =calcAngle(bgJoystick->getPosition(), joystick->getPosition()) - 90;
+    
+    
+}
+
 
 void HelloWorld::onTouchesEnded(const std::vector<Touch*>& touches, Event* event)
 {
     Point location = touches[0]->getLocation();
     locationToMove = location;
 
-    allowMovement = true;
+    allowMovement = false;
     
 }
 
 
 void HelloWorld::update(float dt)
 {
+    
+    
+ //   theIsland->setRotation3D(Vec3(theIsland->getRotation3D().x+1, theIsland->getRotation3D().y +1 , theIsland->getRotation3D().z +1));
     //need to be moved in a seperate class// for test only!
 
     CameraController::getMainCamera()->setPositionX(CameraController::getMainCamera()->getPositionX() + (Player::getInstance()->getPositionX()- CameraController::getMainCamera()->getPositionX())* dt * 3);
@@ -92,58 +157,91 @@ void HelloWorld::update(float dt)
     CameraController::getMainCamera()->setPositionZ(CameraController::getMainCamera()->getPositionZ() + (400 - CameraController::getMainCamera()->getPositionZ())* dt);
     CameraController::getMainCamera()->lookAt(Vec3(CameraController::getMainCamera()->getPositionX() ,  CameraController::getMainCamera()->getPositionY() +200 ,0 ), Vec3(0, 1, 0));
 
+//    
+//    if(Player::getInstance()->getPosition().distance(locationToMove) > 100)
+//    {
+//        if(allowMovement)
+//        {
+//            Player::getInstance()->setPosition(Player::getInstance()->getPosition().x + 40/25*sinf(CC_DEGREES_TO_RADIANS(Player::getInstance()->getRotation()+180)) , Player::getInstance()->getPosition().y + 40/25 *cosf(CC_DEGREES_TO_RADIANS(Player::getInstance()->getRotation()+180)));
+//        
+//            
+//            
+//            Player::getInstance()->setRotation(modulus(Player::getInstance()->getRotation()+360, 360));
+//            
+//            
+//            float playerRotation = calcAngle(Player::getInstance()->getPosition(), locationToMove);
+//           
+//            if(playerRotation -Player::getInstance()->getRotation() > 200)
+//            {
+//                int i = (int)(playerRotation - Player::getInstance()->getRotation())/360;
+//                playerRotation = playerRotation - (360*(i+1));
+//                
+//                
+//            }
+//            if(playerRotation - Player::getInstance()->getRotation() < -200)
+//            {
+//                int i = (int)(playerRotation - Player::getInstance()->getRotation())/360;
+//                
+//                playerRotation = playerRotation + (360*(i+1));
+//                
+//            }
+//
+//            
+//            float rotationSpeed = 200;
+//            
+//            float angleDiffence = -Player::getInstance()->getRotation()+ playerRotation;
+//
+//            if ( fabs(angleDiffence) < rotationSpeed*dt ) // finished rotation
+//            {
+//                Player::getInstance()->setRotation(playerRotation);
+//            }
+//            
+//            else if(angleDiffence  > 0){
+//                Player::getInstance()->setRotation(Player::getInstance()->getRotation()+ rotationSpeed*dt );
+//                
+//            }
+//            else{
+//                Player::getInstance()->setRotation(Player::getInstance()->getRotation()- rotationSpeed*dt );
+//                
+//            }
+//        }
+//    }
+//    else{
+//        allowMovement = false;
+//    }
     
-    if(Player::getInstance()->getPosition().distance(locationToMove) > 100)
+    
+    
+    float x = 2.76*(-bgJoystick->getPosition().x + joystick->getPosition().x)/bgJoystick->getPosition().distance(joystick->getPosition());
+    float y = 2.76*(-bgJoystick->getPosition().y + joystick->getPosition().y)/bgJoystick->getPosition().distance(joystick->getPosition());
+    if(allowMovement){
+    Player::getInstance()->setPosition(Vec2(Player::getInstance()->getPosition().x + x ,Player::getInstance()->getPosition().y + y));
+    }
+
+    
+    Player::getInstance()->setRotation3D(Vec3(Player::getInstance()->getRotation3D().x,Player::getInstance()->getRotation3D().y,modulus(Player::getInstance()->getRotation3D().z,360)));
+    
+    Point playerPosition = Player::getInstance()->getPosition();
+    if(playerRotation -Player::getInstance()->getRotation3D().z > 200)
     {
-        if(allowMovement)
-        {
-            Player::getInstance()->setPosition(Player::getInstance()->getPosition().x + 40/25*sinf(CC_DEGREES_TO_RADIANS(Player::getInstance()->getRotation()+180)) , Player::getInstance()->getPosition().y + 40/25 *cosf(CC_DEGREES_TO_RADIANS(Player::getInstance()->getRotation()+180)));
+        int i = (int)(playerRotation - Player::getInstance()->getRotation3D().z)/360;
+        playerRotation = playerRotation - (360*(i+1));
         
-            
-            
-            Player::getInstance()->setRotation(modulus(Player::getInstance()->getRotation()+360, 360));
-            
-            
-            float playerRotation = calcAngle(Player::getInstance()->getPosition(), locationToMove);
-           
-            if(playerRotation -Player::getInstance()->getRotation() > 200)
-            {
-                int i = (int)(playerRotation - Player::getInstance()->getRotation())/360;
-                playerRotation = playerRotation - (360*(i+1));
-                
-                
-            }
-            if(playerRotation - Player::getInstance()->getRotation() < -200)
-            {
-                int i = (int)(playerRotation - Player::getInstance()->getRotation())/360;
-                
-                playerRotation = playerRotation + (360*(i+1));
-                
-            }
-
-            
-            float rotationSpeed = 200;
-            
-            float angleDiffence = -Player::getInstance()->getRotation()+ playerRotation;
-
-            if ( fabs(angleDiffence) < rotationSpeed*dt ) // finished rotation
-            {
-                Player::getInstance()->setRotation(playerRotation);
-            }
-            
-            else if(angleDiffence  > 0){
-                Player::getInstance()->setRotation(Player::getInstance()->getRotation()+ rotationSpeed*dt );
-                
-            }
-            else{
-                Player::getInstance()->setRotation(Player::getInstance()->getRotation()- rotationSpeed*dt );
-                
-            }
-        }
+        
     }
-    else{
-        allowMovement = false;
+    if(playerRotation - Player::getInstance()->getRotation3D().z < -200)
+    {
+        int i = (int)(playerRotation - Player::getInstance()->getRotation3D().z)/360;
+        
+        playerRotation = playerRotation + (360*(i+1));
+        
     }
+    
+    
+    
+    Player::getInstance()->setRotation3D(Vec3(Player::getInstance()->getRotation3D().x,0,Player::getInstance()->getRotation3D().z + (playerRotation- Player::getInstance()->getRotation3D().z)* dt*8));
+    
+
 
 }
 
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -11,7 +11,13 @@ private:
     Sprite3D *theIsland;
     void InitIsland();
     Size winSize;
+    Point touchLocation;
+    float playerRotation;
     
+    Layer *hudLayer;
+    Sprite *bgJoystick;
+    Sprite *joystick;
+
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
@@ -19,7 +25,9 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
     void update(float dt);
-    
+    void onTouchesBegan(const std::vector<Touch*>& touches, Event* event);
+    void onTouchesMoved(const std::vector<Touch*>& touches, Event* event);
+
     void onTouchesEnded(const std::vector<Touch*>& touches, Event* event);//move to another class
     Point locationToMove;
     bool allowMovement;
